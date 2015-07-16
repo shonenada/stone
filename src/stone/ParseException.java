1b502ae127e856c7a0ac81651481d1cc653163b9
shonenada <shonenada@gmail.com>
2015-07-16 15:17:36 +0000

    add ParseException.java

---
+++ src/stone/ParseException.java
@@ -0,0 +1,27 @@
+package stone;
+
+import java.io.IOException;
+
+public class ParseException extends Exception {
+    public ParseException(Token t) {
+        this("", t);
+    }
+
+    public ParseException(String m, Token t) {
+        super("syntax error around " + location(t) + ". " + msg);
+    }
+
+    private static String location(Token t) {
+        if (t == Token.EOF) {
+            return "tht last line";
+        } else {
+            return "\"" + t.getText() "\" at line " + t.getLineNumber();
+        }
+    }
+    public ParseException (IOException e) {
+        super(e);
+    }
+    public ParseException(String msg) {
+        super(msg);
+    }
+}
