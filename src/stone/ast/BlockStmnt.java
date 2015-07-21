7bf4455c871e621312f28b64366e406edffee410
shonenada <shonenada@gmail.com>
2015-07-21 13:51:35 +0000

    add AST java files

---
+++ src/stone/ast/BlockStmnt.java
@@ -0,0 +1,8 @@
+package stone.ast;
+import java.util.List;
+
+public class BlockStmnt extends ASTList {
+    public BlockStmnt(List<ASTList> c) {
+        super(c);
+    }
+}
---
+++ src/stone/ast/NullStmnt.java
@@ -0,0 +1,8 @@
+package stone.ast;
+import java.util.List;
+
+public class NullStmnt extends ASTList {
+    public NullStmnt (List<ASTree> c) {
+        super(c);
+    }
+}
